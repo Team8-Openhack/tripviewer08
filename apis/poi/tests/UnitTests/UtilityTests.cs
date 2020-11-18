dd5596b8fb3a4f79908bd3390c59dfcdce127e11
husamhilal <husamhilal@hotmail.com>
2020-11-18 15:18:38 -0500

    Update UtilityTests.cs
    update value to 1995

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -8,7 +8,7 @@ namespace UnitTests
         [Fact]
         public void TestLoggingEvents()
         {
-            Assert.Equal(1999, LoggingEvents.Healthcheck);
+            Assert.Equal(1995, LoggingEvents.Healthcheck);
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
             Assert.Equal(2002, LoggingEvents.GetPOIByTripID);
